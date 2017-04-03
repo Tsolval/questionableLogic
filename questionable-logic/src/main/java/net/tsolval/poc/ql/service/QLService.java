71e778e2a4082706f1e663bd7a868326dfe33ee4
dexter.green.ii <dextergreenii@gmail.com>
2017-04-03 11:20:15 -0500

    Updated to pass question Ids into Question constructor

--- questionable-logic/src/main/java/net/tsolval/poc/ql/service/QLService.java
+++ questionable-logic/src/main/java/net/tsolval/poc/ql/service/QLService.java
@@ -24,10 +24,10 @@ public class QLService {
 	public List<Question> getQuestions(String questionGroup) {
 		// TODO: Replace this test implementation.
 		List<Question> questions = new ArrayList<Question>();
-		Question q1 = new Question("Is this a question?", ResponseType.BOOLEAN);
-		Question q2 = new Question("You're right! How does that make you feel?", ResponseType.TEXTBOX,
+		Question q1 = new Question("1", "Is this a question?", ResponseType.BOOLEAN);
+		Question q2 = new Question("2", "You're right! How does that make you feel?", ResponseType.TEXTBOX,
 				new Conditional(q1, "YES"));
-		Question q3 = new Question("Of course it's a question! How could you get that wrong!?", ResponseType.TEXTAREA,
+		Question q3 = new Question("3", "Of course it's a question! How could you get that wrong!?", ResponseType.TEXTAREA,
 				new Conditional(q1, "NO"));
 		q1.setChildren(q2, q3);
 		questions.add(q1);
