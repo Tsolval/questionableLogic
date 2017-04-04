481cdb8ad731f7689c52cd2a2d5ac3fe7f99e7de
dexter.green.ii <dextergreenii@gmail.com>
2017-04-04 11:03:49 -0500

    Updated to use getNextQuestion from QLService

--- questionable-logic/src/main/java/net/tsolval/poc/ql/controller/QLController.java
+++ questionable-logic/src/main/java/net/tsolval/poc/ql/controller/QLController.java
@@ -1,5 +1,7 @@
 package net.tsolval.poc.ql.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +10,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.tsolval.poc.ql.entity.Question;
 import net.tsolval.poc.ql.entity.Survey;
 import net.tsolval.poc.ql.service.QLService;
-import net.tsoval.poc.ql.form.FormData;
 
 /**
  * Questionable Logic Controller displays questions and handles answer input.
@@ -21,41 +23,52 @@ import net.tsoval.poc.ql.form.FormData;
  * @author walker.d.adams
  */
 @Controller
+@SessionAttributes({ "survey" })
 public class QLController {
 	@Autowired
 	private QLService service;
 
 	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "Thank You") String name,
 			Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
 	}
 
-	@RequestMapping(value="/survey/{surveyName}", method = RequestMethod.GET)
+	@RequestMapping(value = "/survey/{surveyName}", method = RequestMethod.GET)
 	public ModelAndView doSurvey() {
-		System.out.println("first Spring method");
 		Survey survey = service.getNewSurvey("testGroup");
+
 		ModelAndView mav = new ModelAndView("survey", "survey", survey);
-		
-		FormData formData = new FormData();
-		
-		for(Question question : survey.getQuestions()){
-			formData.getQuestionsMap().put(question.getQuestionId(), question);
-		}
-		
-		mav.addObject("formData", formData);
+		Question question = survey.getNextQuestion();
+		mav.addObject("question", question);
+
 		return mav;
 	}
-	
+
 	@RequestMapping(value = "/survey/{surveyName}/continue", method = RequestMethod.POST)
-	public ModelAndView doSurvey(@ModelAttribute("formData") FormData formData, BindingResult bindingResult) {
-		for(String q : formData.getQuestionsMap().keySet()){
-			System.out.println(formData.getQuestionsMap().get(q));
-			System.out.println(formData.getQuestionsMap().get(q).getResponse().getAnswer());
+	public ModelAndView doSurvey(@ModelAttribute("survey") Survey survey, @ModelAttribute("question") Question q,
+			BindingResult bindingResult, HttpServletRequest request) {
+
+		request.getParameter("questionId");
+		System.out.println("second method: ");
+
+		System.out.println("q: " + q);
+		for (Question question1 : survey.getQuestions()) {
+			if (question1.getQuestionId().equals(q.getQuestionId())) {
+				question1.getResponse().setAnswer(q.getResponse().getAnswer());
+			}
 		}
-		System.out.println("second method: " + formData.getQuestionsMap());
-		Survey survey = service.getNewSurvey("testGroup");
-		return new ModelAndView("survey", "survey", survey);
+
+		Question question = survey.getNextQuestion();
+		ModelAndView mav = null;
+		if (question == null) {
+			mav = new ModelAndView("redirect:/greeting");
+		} else {
+		mav = new ModelAndView("survey", "survey", survey);
+		mav.addObject("question", question);
+		System.out.println(question);
+		}
+		return mav;
 	}
 }
