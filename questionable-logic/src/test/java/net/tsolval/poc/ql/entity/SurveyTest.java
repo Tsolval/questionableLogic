12bd03e84c0ce295d110085412ae6a2a9d7554d8
dextros <dextergreenii@gmail.com>
2017-04-06 17:13:04 -0500

    Re-factored unit test for survey

--- questionable-logic/src/test/java/net/tsolval/poc/ql/entity/SurveyTest.java
+++ questionable-logic/src/test/java/net/tsolval/poc/ql/entity/SurveyTest.java
@@ -1,10 +1,11 @@
 package net.tsolval.poc.ql.entity;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.hamcrest.CoreMatchers;
@@ -28,13 +29,21 @@ public class SurveyTest {
 	@Before
 	public void setupSurvey() {
 		survey = new Survey();
+		List<Question> questions = new ArrayList<Question>();
 		Question q1 = new Question("Q1", "Is this a question?", ResponseType.BOOLEAN);
 		Question q2 = new Question("Q2", "You're right! How does that make you feel?", ResponseType.TEXTBOX,
 				new Conditional(q1, "YES"));
 		Question q3 = new Question("Q3", "Of course it's a question! How could you get that wrong!?",
 				ResponseType.TEXTAREA, new Conditional(q1, "NO"));
 		q1.setChildren(q2, q3);
-		survey.addQuestion(q1);
+		questions.add(q1);
+		survey.setQuestions(questions);
+
+		Map<String, Question> questionMap = new HashMap<String, Question>();
+		questionMap.put(q1.getQuestionId(), q1);
+		questionMap.put(q2.getQuestionId(), q2);
+		questionMap.put(q3.getQuestionId(), q3);
+		survey.setQuestionMap(questionMap);
 	}
 
 	/**
@@ -42,25 +51,30 @@ public class SurveyTest {
 	 * {@link net.tsolval.poc.ql.entity.Survey#getNextQuestion(net.tsolval.poc.ql.entity.Question)}.
 	 */
 	@Test
-	public void testGetNextQuestion() throws Exception {
-		assertThat(survey.getNextQuestion().toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q1"),
+	public void testGetNextQuestionWhereAnswerYes() throws Exception {
+		Question question = survey.getNextQuestion();
+		question.getResponse().setAnswer("YES");
+		
+		assertThat(question.toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q1"),
 				CoreMatchers.containsString("parent=null"), CoreMatchers.containsString("children=[Q2, Q3]")));
 		assertThat(survey.getNextQuestion().toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q2"),
 				CoreMatchers.containsString("parent=Q1"), CoreMatchers.containsString("children=[]")));
+		assertNull(survey.getNextQuestion());
+	}
+	
+	/**
+	 * Test method for
+	 * {@link net.tsolval.poc.ql.entity.Survey#getNextQuestion(net.tsolval.poc.ql.entity.Question)}.
+	 */
+	@Test
+	public void testGetNextQuestionWhereAnswerNo() throws Exception {
+		Question question = survey.getNextQuestion();
+		question.getResponse().setAnswer("NO");
+		
+		assertThat(question.toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q1"),
+				CoreMatchers.containsString("parent=null"), CoreMatchers.containsString("children=[Q2, Q3]")));
 		assertThat(survey.getNextQuestion().toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q3"),
 				CoreMatchers.containsString("parent=Q1"), CoreMatchers.containsString("children=[]")));
 		assertNull(survey.getNextQuestion());
-		assertThat(survey.getNextQuestion().toString(), CoreMatchers.allOf(CoreMatchers.containsString("id=Q1"),
-				CoreMatchers.containsString("parent=null"), CoreMatchers.containsString("children=[Q2, Q3]")));
-	}
-
-	@Test
-	public void testQuestionIteraton() {
-		Map<String, Question> questionMap = new HashMap<String, Question>();
-		while (survey.hasNextQuestion()) {
-			Question q = survey.getNextQuestion();
-			questionMap.put(q.getQuestionId(), q);
-		}
-		assertEquals(3, questionMap.keySet().size());
 	}
 }
